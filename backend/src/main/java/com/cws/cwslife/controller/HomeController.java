b5edb93fa074337313b192e1eb21062be5a9038b
meghlesh <meghachaudhari24@gmail.com>
2026-06-17 16:13:42 +0530

    Add HomeController with a home endpoint

---
+++ backend/src/main/java/com/cws/cwslife/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.cws.cwslife.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Life API Running Successfully";
+    }
+}
